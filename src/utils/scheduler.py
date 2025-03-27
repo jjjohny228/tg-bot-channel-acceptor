f2b5b2913ad7f3aae37278cf8deec5fdb9cf1e7a
jjjohny228 <gggleb13@gmail.com>
2025-03-27 23:02:37 +0200

    Fixed scheduler

--- src/utils/scheduler.py
+++ src/utils/scheduler.py
@@ -2,5 +2,5 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 def schedule_func(func):
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(func=func, trigger="cron", hour=12, minuts=5)
+    scheduler.add_job(func=func, trigger="cron", hour=12, minute=5)
     scheduler.start()
